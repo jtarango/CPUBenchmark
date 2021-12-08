a8f5710240ecdf3250db23c1947963905f06cd42
Joe <41123949+jtarango@users.noreply.github.com>
2021-12-08 12:27:54 -0700

    Create cpuBenchmarkParallel.cpp

---
+++ src/cpuBenchmarkParallel.cpp
@@ -0,0 +1,37 @@
+/*
+ * Written by Joseph Tarango. The original work was to develop a dynamic data
+ * type for precision related code in embedded processors. Joseph
+ * Tarango webpages can be found at http://www.josephtarango.com
+ *
+ *THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ =============================================================================*/
+// #pragma once // Only used in header files.
+
+#ifndef __cplusplus
+extern "C++" {
+#endif // __cplusplus
+
+#ifndef _CPUBENCHMARKPARALLEL_CPP_
+#define _CPUBENCHMARKPARALLEL_CPP_
+
+
+int main(int argc, char *argv[])
+{
+ 
+  return 0;
+}
+
+#endif // _CPUBENCHMARKPARALLEL_CPP_
+
+#ifndef __cplusplus
+}
+#endif // __cplusplus
